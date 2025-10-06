4750ad02b05e090b131de6bc44cc04d8531a8a29
van-kzr <van.kzr@gmail.com>
2025-10-06 15:33:13 +0700

    ssss

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -44,62 +44,61 @@ const ProjectItem = ({ project, i }: { project: Project; i: number }) => {
 
 
   return (
-    <div className={`h-40 xl:h-56 relative w-full `}>
+    <div className={`xl:h-56 relative w-full `}>
         <motion.div
         ref={ref}
         key={i}
-        className={`h-full translate-x-10 w-full flex flex-col gap-5 xl:gap-24 ${
+        className={`h-full w-full flex flex-col gap-5 xl:gap-24 ${
             i % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse"
         }`}
         initial={{ opacity: 0, x: i % 2 === 0 ? 100 : -100 }}
         animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: i % 2 === 0 ? 100 : -100}}
         transition={{ duration: 0.6, ease: "easeOut", delay: i * 0.2 }}
         >
-        {/* Gambar */}
-        <div className="w-full h-full 2xl:w-3xl">
-            <img
-            src={project.image}
-            alt={project.name}
-            className="w-full object-cover h-full rounded-sm"
-            />
-        </div>
+			<div className="w-full h-full 2xl:w-3xl">
+				<img
+				src={project.image}
+				alt={project.name}
+				className="w-full object-cover h-full rounded-sm"
+				/>
+			</div>
 
-        {/* Konten */}
-        <div className="w-full flex flex-col gap-5 font-adamina">
-            <div className="relative w-full flex flex-row gap-5 justify-center items-center">
-            <div className="absolute right-0 flex items-center justify-between">
-                <img
-                src="https://skillicons.dev/icons?i=github"
-                alt="GitHub"
-                className="h-6 w-6 md:h-8 md:w-8"
-                />
-            </div>
-            <h1 className="text-center text-sm lg:text-xl">{project.name}</h1>
-            </div>
+			{/* Konten */}
+			<div className="w-full flex flex-col gap-5 font-adamina">
+				<div className="relative w-full flex flex-row gap-5 justify-center items-center">
+				<div className="absolute right-0 flex items-center justify-between">
+					<img
+					src="https://skillicons.dev/icons?i=github"
+					alt="GitHub"
+					className="h-6 w-6 md:h-8 md:w-8"
+					/>
+				</div>
+				<h1 className="text-center text-sm lg:text-xl">{project.name}</h1>
+				</div>
 
-            <p className="text-justify text-xs lg:text-sm xl:text-lg overflow-hidden">
-            {project.description}
-            </p>
+				<p className="text-justify text-xs lg:text-sm xl:text-lg overflow-hidden">
+				{project.description}
+				</p>
 
-            {/* Tech Stack */}
-            <div className="w-full flex">
-            <div className="flex flex-col text-xs gap-2 lg:flex-row items-center flex-wrap">
-                <h3 className="text-sm xl:text-lg">Tech Stack:</h3>
-                <div className="flex gap-2 flex-wrap justify-center">
-                {project.tech.map((tech, idx) => (
-                    <div
-                    key={idx}
-                    className="flex gap-2 bg-[#1D1D1D] px-3 py-2 rounded-sm"
-                    >
-                    <h2 className="font-adamina text-xs min-2xl:text-lg">
-                        {tech}
-                    </h2>
-                    </div>
-                ))}
-                </div>
-            </div>
-            </div>
-        </div>
+				{/* Tech Stack */}
+				<div className="w-full flex justify-center xl:justify-start">
+				<div className="flex flex-col text-xs gap-2 lg:flex-row items-center flex-wrap">
+					<h3 className="text-sm xl:text-lg">Tech Stack:</h3>
+					<div className="flex gap-2 flex-wrap justify-center">
+					{project.tech.map((tech, idx) => (
+						<div
+						key={idx}
+						className="flex gap-2 bg-[#1D1D1D] px-3 py-2 rounded-sm"
+						>
+						<h2 className="font-adamina text-xs min-2xl:text-lg">
+							{tech}
+						</h2>
+						</div>
+					))}
+					</div>
+				</div>
+				</div>
+			</div>
         </motion.div>
     </div>
     
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -40,13 +40,13 @@ const Home = () => {
 	return (
 		<div className='h-full page-scroll'>
 			
-				<div className="w-full flex flex-col lg:gap-32 items-center ">
+				<div className="w-full flex flex-col lg:gap-24 items-center ">
 					<div className="flex items-center h-28 gap-3">
 						<h1 className="text-xl xl:text-5xl font-port_lligat_slab font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500">What I’ve Been Working On</h1>
 					</div>
 
 					<div className='w-full xl:w-7xl'>
-						<div className="h-full w-full flex flex-col items-center justify-center gap-16 text-neutral-300 xl:gap-28">
+						<div className="h-full w-full flex flex-col items-center justify-center text-neutral-300 gap-20 xl:gap-28">
 						{projects.map((project, i) => (
 							<ProjectItem key={i} project={project} i={i} />
 						))}
